bfd0f70c0a28bba486e8bccad81e3643e59c7cd3
Alex Kachanov <ka4anoov@gmail.com>
2022-09-07 21:53:32 +0300

    3_commit

--- PYT_Senior.py
+++ PYT_Senior.py
@@ -3,6 +3,6 @@ name = input('Text your name')
 if name == 'Alex':
     print (f'Hi {name} Its Test code for Git')
 else:
-    print('Where is Allex?')
+    print('Where is Alex?')
 
 print('Something for commit')
