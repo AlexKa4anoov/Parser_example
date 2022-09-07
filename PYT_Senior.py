08bafd8cc497028406b6d71622bd5309a0440c1a
Alex Kachanov <ka4anoov@gmail.com>
2022-09-07 21:49:45 +0300

    make A capital in alex and + exclamation

--- PYT_Senior.py
+++ PYT_Senior.py
@@ -1,6 +1,8 @@
 name = input('Text your name')
+
 if name == 'Alex':
     print (f'Hi {name} Its Test code for Git')
 else:
-    print('Where is alex?')
+    print('Where is Allex?')
 
+print('Something for commit')
